e3f4271ca1cd292af08ccaf3adc1f7398c0d14d0
Ben <WBHankins93@Bens-MacBook-Air.local>
2018-09-25 09:50:39 -0500

    calculator completed

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-console.log('hello')
+$('.allClear').on('click', () => {
+  $('input').val('0');
+});
+
+$('.equals').on('click', () =>  {
+  let eq = $('input').val();
+  eq = eq.slice(0, eq.length -1);
+  eq = eval(eq);
+  $('input').val(eq);
+});
